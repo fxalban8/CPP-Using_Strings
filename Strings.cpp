308e667f66f0cf003bd4b2c2d2de47f0c7c862c3
fxalban08 <francisco.alban@epn.edu.ec>
2020-05-30 09:47:10 -0500

    Concatenating strings

--- Strings.cpp
+++ Strings.cpp
@@ -8,5 +8,8 @@ int main(){
 	string Y=", I'm greeting you";	//declaring a second string variable
 	cout<<X<<Y<<endl;	//printing two strings with a single cout command
 
+	string Z=X+Y;		//concatenating strings
+	cout<<Z<<endl;
+
 	return 0;
 }
