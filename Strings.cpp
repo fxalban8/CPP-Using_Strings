255d700984d2b9251e57a2fa8f99863a367722c4
fxalban08 <francisco.alban@epn.edu.ec>
2020-05-30 09:46:23 -0500

    Printing two strings in a COUT-COMMAND line

--- Strings.cpp
+++ Strings.cpp
@@ -5,6 +5,8 @@ int main(){
 
 	string X= "Hello this is a string";
 	cout<<X<<endl;		//Printing a string
+	string Y=", I'm greeting you";	//declaring a second string variable
+	cout<<X<<Y<<endl;	//printing two strings with a single cout command
 
 	return 0;
 }
